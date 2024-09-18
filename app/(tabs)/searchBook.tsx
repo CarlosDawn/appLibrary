c7d262a1ce5af7a8ca573f0a5ac5015c1d03d29d
CarlosDawn <maincontaYuri@gmail.com>
2024-09-18 19:17:40 -0300

    Tentando Navegar entre Telas
    navagation.navagate não funciona, e ainda estou tentando passar como parâmetro o id do Livro cadastrado.

--- app/(tabs)/searchBook.tsx
+++ app/(tabs)/searchBook.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from 'react';
-import { StyleSheet, View, FlatList, TextInput } from 'react-native';
+import { StyleSheet, View, FlatList, TextInput, Button } from 'react-native';
 
 import { useDatabase, LivroDataBse } from '@/database/useDataBase';
 
@@ -8,20 +8,30 @@ import { ThemedText } from '@/components/ThemedText';
 
 import { LivroData } from '@/components/bucandoLivros'
 
-import { NavigationContainer  } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
 import areaBook from '@/app/(tabs)/areaBook';
 
+function irParaAreaLivro (itemID: number) {
 
-export default function SearchScreen() {
+  const navigation = useNavigation();
+
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <ThemedText>Home Screen</ThemedText>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('areaBook', itemID)}
+      />
+    </View>
+  );
+};
+
+export default function SearchScreen(){
   const [livros, setLivros] = useState<LivroDataBse[]>([])
   const [busca, setBusca] = useState("")
 
-  //const navigation = NavigationContainer ();
   const Stack = createStackNavigator();
-  /*const irParaAreaLivro = (itemId: number ) => {
-    navigation.navigate('areaBook', {itemId});
-  };*/
 
   const livroDatabase = useDatabase();
 
@@ -41,9 +51,12 @@ export default function SearchScreen() {
   return (
     
     <View style={styles.titleContainer}>
-
       <ThemedText style={styles.textoStyle} type='title'>Welcome! Serach Books Area</ThemedText>
       <HelloWave />
+
+      <Stack.Navigator>
+        <Stack.Screen name='areaBook' component={areaBook}></Stack.Screen>  
+      </Stack.Navigator>
 
       <TextInput style={{height: 40, borderWidth: 1, borderColor: "#999", borderRadius: 9, paddingHorizontal:100}} placeholder='Buscar' onChangeText={setBusca}/>
 
@@ -52,7 +65,9 @@ export default function SearchScreen() {
         renderItem={({item}) => < LivroData data={item} onPress={() => irParaAreaLivro(item.id)}/>}
         keyExtractor={(item) => String(item.id)}
       />
+      
     </View>  
+    
   );
 }
 
